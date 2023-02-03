73181b31c139bb0046a81faa479043ea1a650dbc
OFC HADES-BOT-MD <100550974+OFC-YOVANI@users.noreply.github.com>
2023-02-02 23:25:14 -0600

    Delete plugins directory

--- plugins/owner-unbanchat.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m) => {
-global.db.data.chats[m.chat].isBanned = false
-m.reply('*│✔️│𝚎𝚜𝚝𝚎 𝚌𝚑𝚊𝚝 𝚏𝚞𝚎 𝚍𝚎𝚜 𝚋𝚊𝚗𝚎𝚊𝚍𝚘 𝚎𝚡𝚒𝚝𝚘𝚜𝚊𝚖𝚎𝚗𝚝𝚎🌴*')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.rowner = true
-export default handler
