4cce17225686f8389ea484f9deb9ff59772c7721
DEYNER BOT-MD <102321484+Deyner34@users.noreply.github.com>
2023-06-16 11:28:10 -0500

    Update creator.js
    Ya

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,6 +5,6 @@ function handler(m) {
 handler.help = ['creador', 'programador']
 handler.tags = ['info']
 
-handler.command = /^(yovani)$/i
+handler.command = /^(Deyner)$/i
 
 export default handler
